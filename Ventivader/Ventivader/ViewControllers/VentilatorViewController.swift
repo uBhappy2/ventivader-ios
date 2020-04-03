4842c4a53296e7df486a259f2fd06922d9e7549a
uBhappy2 <rao.amit@gmail.com>
2020-04-03 11:06:34 -0700

    Checking in initial code and assets
    - initial code
    - storyboard
    - app icon assets

---
+++ Ventivader/Ventivader/ViewControllers/VentilatorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VentilatorViewController.swift
+//  Ventivader
+//
+//  Created by Amit Rao on 4/3/20.
+//  Copyright © 2020 calculator. All rights reserved.
+//
+
+import UIKit
+
+class VentilatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
